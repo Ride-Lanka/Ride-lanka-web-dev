6c891b21a071af0e30f7444c2feae9a8d9d6d0bf
SithilSemitha <sithilsemithad@gmail.com>
2026-03-11 19:53:35 +0530

    feat: Updated index

---
+++ src/components/screens/index.js
@@ -0,0 +1,12 @@
+export { default as SplashScreen } from "./SplashScreen";
+export { default as AuthScreen } from "./AuthScreen";
+export { default as LoginForm } from "./LoginForm";
+export { default as SignUpForm } from "./SignUpForm";
+export { default as HomeScreen } from "./HomeScreen";
+export { default as DetailScreen } from "./DetailScreen";
+export { default as TripsScreen } from "./TripsScreen";
+export { default as WishlistScreen } from "./WishlistScreen";
+export { default as ProfileScreen } from "./ProfileScreen";
+export { default as CommunityScreen } from "./CommunityScreen";
+export { default as NotificationsScreen } from "./NotificationsScreen";
+export { default as SettingsScreen } from "./SettingsScreen";
